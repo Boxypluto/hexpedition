e07c662ced0b683b3dc93fc3e100f583d73abaf6
Boxypluto <122708916+Boxypluto@users.noreply.github.com>
2025-11-11 12:06:05 -0500

    Turned on Phys Interp, added Impio, fixed hit effect

--- Game/Enemy/enemy_hit_effect.gd
+++ Game/Enemy/enemy_hit_effect.gd
@@ -1,9 +1,12 @@
 extends Sprite2D
 
 func _ready() -> void:
+	physics_interpolation_mode = Node.PHYSICS_INTERPOLATION_MODE_OFF
 	scale.x = randf_range(0.8, 1.2)
 	scale.y = scale.x
 	rotation = randf_range(0.0, TAU)
+	await get_tree().create_timer(0.05).timeout
+	physics_interpolation_mode = Node.PHYSICS_INTERPOLATION_MODE_INHERIT
 
 func _on_timer_timeout() -> void:
 	queue_free()
